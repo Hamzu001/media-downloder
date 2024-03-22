fb139ff5926d6eba27929fbe4f7c62095937f4f0
Hamza Zahid <zahidhamza001@gmail.com>
2024-03-22 22:15:28 +0500

    edit responce

--- src/app.js
+++ src/app.js
@@ -8,25 +8,28 @@ app.use(express.json());
 
 app.post("/media-api/pinterest", cors(corsOptions), async (req, res) => {
   const { url } = req.body;
-  if (!url) return res.status(400).send("Enter video url");
+  if (!url) return res.status(400).json({message:"Enter video url"});
 
   if (["pinterest.com", "pin.it"].some((u) => url?.match(u)) != true) {
-    return res.status(400).send("Enter pinterest video url");
+    return res.status(400).json({message:"Enter pinterest video url"});
   }
 
   try {
     const response = await fetch(url);
     if (response.status !== 200) {
-      return res.status(400).send("invalid url");
+      return res.status(400).json({message:"Invalid url"});
     }
 
     const body = await response.text();
     const video = new JSDOM(body).window.document.getElementsByTagName("video")[0].src;
+    if (!video) {
+      return res.status(400).json({message:"Invalid url"});
+    }
+    
     const videoUrl = video.replace("/hls/", "/720p/").replace(".m3u8", ".mp4");
-
     res.status(200).json({ url: videoUrl });
   } catch (error) {
-    res.status(400).send("Api does not responce");
+    res.status(400).json({message:"Api does not responce"});
     console.log(error);
   }
 });
